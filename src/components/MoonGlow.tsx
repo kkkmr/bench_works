f34ee6d4a6c318858b27752e1750f4b2b37910ca
kkkmr <karthikmarapaka@gmail.com>
2025-07-08 19:19:14 +0530

    Removed extra files and folders

--- src/components/MoonGlow.tsx
+++
@@ -1,5 +0,0 @@
-import "./MoonGlow.css";
-
-export default function MoonGlow() {
-  return <div className="moon-container"></div>
-}
--- src/hooks/useAppSelector.tsx
+++
@@ -1,5 +0,0 @@
-import { useSelector, type TypedUseSelectorHook } from "react-redux";
-import type { RootState } from "../store";
-
-const useAppSelector: TypedUseSelectorHook<RootState>=useSelector;
-export default useAppSelector;
